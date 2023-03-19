fbc94b3c23666772fb9d275531f97d7b936cd1bf
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-03-19 19:44:41 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const  currentUser = 'Binil Thomas';
+let welcomeMessage = 'Welcome to Flatbook, ';
+welcomeMessage = `${welcomeMessage} + ${currentUser} +!` ;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting =`Welcome, ${currentUser.slice(0,1)}!`;
+
+
+
+
+
+
+
